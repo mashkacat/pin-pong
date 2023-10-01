eb16596e4d5d516b9d6dedc2b4bf5c959e5a837d
mashkacat <145900843+mashkacat@users.noreply.github.com>
2023-10-01 12:52:57 +0500

    Update pinpong.py

---
+++ pinpong.py
@@ -0,0 +1,100 @@
+#создай игру "Л
+from pygame import *
+'''Необходимые классы'''
+
+
+#класс-родитель для спрайтов
+class GameSprite(sprite.Sprite):
+   #конструктор класса
+    def __init__(self, player_x, player_y, player_speed,width, height,player_image = None):
+        super().__init__()
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = Surface((width,height))
+        self.image.fill((0,0,0))
+        if player_image:
+            self.image = transform.scale(image.load(player_image), (width,height))
+        self.speed = player_speed
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+#класс-наследник для спрайта-игрока (управляется стрелками)
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+#создание персонажей:
+player_l=Player(30,200,4,50,150)
+player_r=Player(500,200,4,50,150)
+ball = GameSprite(200,200,4,50,50,'ball.png')
+#Игровая сцена:
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze")
+window.fill((200,255,255))
+
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+font.init()
+font = font.Font("Font", 35)
+lose1 = font1.render('PLAYER 1 LOSE', True, (180, 0, 0))
+font2 = font.Font('Font', 35)
+lose2 = font2.render('PLAYER 2 LOSE', True,(180,0,0))
+
+
+# money = mixer.Sound('money.ogg')
+# kick = mixer.Sound('kick.ogg')
+speed_x = 4
+speed_y = 4
+
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    if finish != True:
+        window.fill((200,255,255))
+        player_l.update_l()
+        player_r.update_r()
+        player_l.reset()
+        player_r.reset()
+        ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    if ball.rect.y < 0 or ball.rect.y > 450:
+        speed_y *= -1
+    if sprite.collide_rect(player_l,ball) or sprite.collide_rect(player_r,ball):
+        speed_x *= -1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1,(200,200))
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose2,(200,200))
+
+
+    display.update()
+    clock.tick(FPS)
